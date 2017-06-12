b6db258cb58ad4041ea359566b79206295407bd2
Michael J. Simons <michael@simons.ac>
2017-06-12 11:55:18 +0200

    Configure object mapper

--- filmdatabase/src/main/java/de/springbootbuch/reactive/filmstore/Application.java
+++ filmdatabase/src/main/java/de/springbootbuch/reactive/filmstore/Application.java
@@ -1,7 +1,11 @@
 package de.springbootbuch.reactive.filmstore;
 
+import de.springbootbuch.reactive.filmstore.support.ActorConstructorMixIn;
+import de.springbootbuch.reactive.filmstore.support.FilmConstructorMixIn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Part of springbootbuch.de.
@@ -12,5 +16,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	Jackson2ObjectMapperBuilderCustomizer objectMapperCustomizer() {
+		return builder -> {
+			builder.mixIn(Film.class, FilmConstructorMixIn.class);
+			builder.mixIn(Actor.class, ActorConstructorMixIn.class);
+		};
 	}
 }
