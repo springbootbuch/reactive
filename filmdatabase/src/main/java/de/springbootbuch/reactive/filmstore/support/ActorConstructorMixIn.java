3357e2819b84d98d1f3a19eb52922a6a19ffab20
Michael J. Simons <michael@simons.ac>
2017-06-12 11:55:09 +0200

    Add sample data and MixIns

---
+++ filmdatabase/src/main/java/de/springbootbuch/reactive/filmstore/support/ActorConstructorMixIn.java
@@ -0,0 +1,15 @@
+package de.springbootbuch.reactive.filmstore.support;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Part of springbootbuch.de.
+ *
+ * @author Michael J. Simons
+ * @author @rotnroll666
+ */
+public abstract class ActorConstructorMixIn {
+
+	ActorConstructorMixIn(@JsonProperty("firstName") String a, @JsonProperty("lastName") String b) {
+	}
+}
