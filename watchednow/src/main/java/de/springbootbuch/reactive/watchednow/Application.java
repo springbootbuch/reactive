e15881aa46ca9b27b757752ecf62ffad4d0e0659
Michael J. Simons <michael@simons.ac>
2017-06-13 15:52:19 +0200

    Use functional style

--- watchednow/src/main/java/de/springbootbuch/reactive/watchednow/Application.java
+++ watchednow/src/main/java/de/springbootbuch/reactive/watchednow/Application.java
@@ -2,6 +2,11 @@ package de.springbootbuch.reactive.watchednow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * Part of springbootbuch.de.
@@ -14,5 +19,18 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	RouterFunction<?> routerFunction(final FilmWatchedEventRepository filmWatchedEventRepository) {
+		return route(
+			POST("/api/filmWatched"), request -> 
+			ok().body(
+				request.bodyToMono(Film.class)
+					.map(FilmWatchedEvent::new)
+					.flatMap(filmWatchedEventRepository::save), 
+				FilmWatchedEvent.class
+			)
+		);
 	}
 }
