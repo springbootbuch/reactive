7afe318df7b91833809f3cc1e26035c2faaaeed3
Michael J. Simons <michael@simons.ac>
2017-07-07 12:24:56 +0200

    Add demo

--- filmstore/src/main/java/de/springbootbuch/reactive/filmstore/DemoRestController.java
+++ filmstore/src/main/java/de/springbootbuch/reactive/filmstore/DemoRestController.java
@@ -1,8 +1,11 @@
 package de.springbootbuch.reactive.filmstore;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -21,5 +24,15 @@ public class DemoRestController {
 			.flatMap(s -> Mono
 				.just(s + ", " + name + "!\n")
 			);
+	}
+	
+	@PostMapping("/manygreetings")
+	public Flux<String> getGreetings(
+		@RequestBody Mono<String> name
+	) {
+		return name
+			.flatMapMany(n -> Flux.generate(s -> s.next(n)))
+			.zipWith(Flux.range(1, 23))
+			.map(t -> "Hello, " + t.getT1() + "\n");
 	}
 }
