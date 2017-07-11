3c99d42ce6e0baed6cce643bf42e14044374a474
Michael J. Simons <michael@simons.ac>
2017-07-11 17:59:50 +0200

    Remove unused stuff

--- filmstore/src/main/java/de/springbootbuch/reactive/filmstore/SecurityConfig.java
+++ filmstore/src/main/java/de/springbootbuch/reactive/filmstore/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.springbootbuch.reactive.filmstore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
 import org.springframework.security.core.userdetails.MapUserDetailsRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsRepository;
@@ -22,10 +23,6 @@ public class SecurityConfig {
 		return new MapUserDetailsRepository(
 			User.withUsername("spring")
 				.password("boot")
-				.roles("USER")
-				.build(),
-			User.withUsername("michael")
-				.password("simons")
 				.roles("USER")
 				.build()
 		);
